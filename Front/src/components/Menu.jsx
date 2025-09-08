1abdac6de11f08f7fb7173c59fe4fc3239333d4d
saranyuchooyat <66015193@kmitl.ac.th>
2025-09-08 21:22:42 +0700

    update menu btn

--- Front/src/components/Menu.jsx
+++ Front/src/components/Menu.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
+
+
+
+
 function Menu(){
+    const [btn,SetBtn]=useState(false);
+
+    function activeBtn(){
+        
+        if (btn == false){
+            SetBtn(true)
+        }
+        else if (btn == true){
+            SetBtn(false)
+        }
+    }
+
     return(
         <>
             <div className="mt-3 mx-5">
                 <div>
                     <p className="text-[22px] font-bold">System Admin Menu </p>
                     <div className="flex justify-start mt-1">
-                        <button className="menu-btn">System Overview Dashboard</button>
+                        <button className="menu-btn" onClick={activeBtn()}>System Overview Dashboard</button>
                        <button className="menu-btn">Zone Management</button>
                        <button className="menu-btn">User Management</button>
                        <button className="menu-btn">System Health Monitoring</button>
